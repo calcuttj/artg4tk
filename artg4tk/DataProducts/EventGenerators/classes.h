27b106c1b0ee1c0fde1da7a5018d10523dc0d8cb
Hans-Joachim Wenzel <wenzel@woof.fnal.gov>
2019-03-26 15:43:59 -0500

    Fix CMakeLists.txt and mode on array_macros.hh.
    The art library names should be lowercase in the CMakeLists.txt to
    support building using mrb with art in the build area.
    
    All CMakeLists.txt have been updated to use only the external libraries
    actually linked against and used.

--- artg4tk/DataProducts/EventGenerators/classes.h
+++ artg4tk/DataProducts/EventGenerators/classes.h
@@ -1,10 +1,7 @@
-//
-// Build a dictionary.
-//
-#include <vector>
 #include "artg4tk/DataProducts/EventGenerators/GenParticle.hh"
 #include "artg4tk/DataProducts/EventGenerators/GenParticleCollection.hh"
 #include "artg4tk/DataProducts/EventGenerators/PDGCode.hh"
 #include "canvas/Persistency/Common/Wrapper.h"
+#include <vector>
 template class art::PtrVector<artg4tk::GenParticle>;
 template class art::Wrapper<artg4tk::GenParticleCollection>;
--- artg4tk/DataProducts/G4DetectorHits/classes.h
+++ artg4tk/DataProducts/G4DetectorHits/classes.h
@@ -1,9 +1,6 @@
-//
-// Build a dictionary.
-//
-#include <vector>
 #include "artg4tk/DataProducts/G4DetectorHits/ArtG4tkVtx.hh"
 #include "artg4tk/DataProducts/G4DetectorHits/ArtG4tkParticle.hh"
 #include "canvas/Persistency/Common/Wrapper.h"
+#include <vector>
 template class std::vector<artg4tk::ArtG4tkParticle>;
 template class art::Wrapper<artg4tk::ArtG4tkVtx>;
